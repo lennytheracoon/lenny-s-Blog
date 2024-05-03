0a65c85bddc07a8a96698bba57e229ff797d6a9e
mayuras7685 <mayurasodara@gmail.com>
2024-05-03 14:07:52 +0530

    project added

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,16 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Berries Classifier',
+    description: `YOLOv8-powered model to sort berries! 🍓🔍 Easily interact with our raccoon-approved interface on Streamlit. Let's get berry-fying! 🌟`,
+    imgSrc: '/static/images/berries-classifier.png',
+    href: 'https://berries-classifier.streamlit.app/',
   },
 ]
 
