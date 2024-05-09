4e057ff2799fa29a2f6fae5c6a00749c6c008a1b
mayuras7685 <mayurasodara@gmail.com>
2024-05-09 11:03:25 +0530

    Project: Blue count

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,6 +12,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/berries-classifier.png',
     href: 'https://berries-classifier.streamlit.app/',
   },
+  {
+    title: 'Blueberry Counter',
+    description: `Roboflow Universe model to detect and count blueberry!,  🍓🔍 Easily interact with our raccoon-approved interface on Streamlit. Let's get berry-fying! 🌟`,
+    imgSrc: '/static/images/blue-count.png',
+    href: 'https://blue-count.streamlit.app/',
+  },
 ]
 
 export default projectsData
